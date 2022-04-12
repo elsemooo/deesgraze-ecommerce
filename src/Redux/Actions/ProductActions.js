fbd6d98833a995c8ba1cdd7d8bc56dc004ad3462
khalid <mathiaskalid@gmail.com>
2022-04-11 23:20:38 -0500

    fix build

--- src/Redux/Actions/ProductActions.js
+++ src/Redux/Actions/ProductActions.js
@@ -18,24 +18,56 @@ import {
 import axios from "axios";
 import { logout } from "./userActions";
 
-export const listProducts = () =>
-  async (dispatch) => {
-    try {
-      dispatch({ type: PRODUCT_LIST_REQUEST });
-      const { data } = await axios.get(
-        `https://deesgraze.herokuapp.com/api/products`
-      );
-      dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
-    } catch (error) {
-      dispatch({
-        type: PRODUCT_LIST_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
+export const listProducts = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`https://deesgraze.herokuapp.com/api/products`, config);
+
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+      dispatch(logout());
     }
-  };
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload: message,
+    });
+  }
+};
+
+// export const listProducts = () =>
+//   async (dispatch) => {
+//     try {
+//       dispatch({ type: PRODUCT_LIST_REQUEST });
+//       const { data } = await axios.get(
+//         `https://deesgraze.herokuapp.com/api/products`
+//       );
+//       dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+//     } catch (error) {
+//       dispatch({
+//         type: PRODUCT_LIST_FAIL,
+//         payload:
+//           error.response && error.response.data.message
+//             ? error.response.data.message
+//             : error.message,
+//       });
+//     }
+//   };
 
 // export const listProducts = () => async (dispatch, getState) => {
 //   try {
